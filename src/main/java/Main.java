976139b92044757554b6da79427e437cb4abd22c
Brian Coley <briancoley@gmail.com>
2019-04-15 20:02:24 -0500

    Add Initial Files and Structure
    First Commit to add initial starting point and to test unit test and ci

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args)throws Exception {
+
+        
+        System.out.println("Hello Assignment");
+
+
+
+    }
+}
---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    // Test case to test set_points changes vs addStudent
+    @Test
+    public void assertTest() {
+        System.out.println("Testing");
+        assertTrue(true);
+    }
+}

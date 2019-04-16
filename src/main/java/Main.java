a4a182696c91f98ffbb83b097776a7d0c5d8de9a
Brian Coley <briancoley@gmail.com>
2019-04-15 21:10:30 -0500

    Test Gradle UnitTest
    Testing Unit Gradle Test and checkstyle/spotbugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
 package main.java;
 
+/**
+ * Class: Main.
+ * @author bcoley2
+ * @version 20190415
+ */
 public class Main {
 
+    /**
+     * Method: Main This is the main method.
+     * @param args Standard Arguments for a main class
+     * @throws Exception Throws Exception
+     */
     public static void main(String[] args)throws Exception {
 
         
